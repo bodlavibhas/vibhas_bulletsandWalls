4f969c21992b8ed77b6871a099dd73e7098583c6
bodlavibhas <75427455+bodlavibhas@users.noreply.github.com>
2020-12-09 21:28:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# vibhas_bulletsandWalls
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,51 @@
+var wall, thickness;
+var bullet, speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+  thickness=random(22,83);
+  
+  speed=random(55,90);
+  weight=random(400,1500);
+  
+  bullet = createSprite(50, 200, 50, 5);
+  bullet.velocityX = speed;
+  bullet.shapeColor=color(225);
+
+
+  wall = createSprite(1200,200,thickness,height/2);
+  
+  wall.shapeColor=color(80,80,80);
+}
+
+function draw() {
+  background(255,255,255);
+  if(hasCollided(wall, bullet))
+  {
+    bullet.velocityX=0;
+    var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+  }
+
+  if(damage>10)
+  {
+    wall.shapeColor=color(225,0,0);
+  }
+
+  if(damage<10)
+  {
+    wall.shapeColor=color(0,225,0);
+  }
+  
+  drawSprites(); 
+}
+ function hasCollided(bullet, wall){
+   bulletRightEdge=bullet.x + bullet.width;
+   wallLeftEdge= wall.x;
+   if (bulletRightEdge>=wallLeftEdge)
+   {
+     return true
+   }
+   return false;
+ }
+  
